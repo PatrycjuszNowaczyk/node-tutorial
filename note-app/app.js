e4b47bdcbf025c6f1d3650c5a8dcc2e9af5b3dc1
PatrycjuszNowaczyk <patrycjusz.nowaczyk@gmail.com>
2022-01-28 20:25:39 +0100

    save work

--- note-app/app.js
+++ note-app/app.js
@@ -8,14 +8,26 @@ const rl = readline.Interface({
     terminal: true
 });
 
-rl.on('line', function(line) {
+oNotes.help();
 
+rl.on('line', function(line) {
     switch (rl.history[0]) {
         case 'add':
-            oNotes.add();
+            // console.log('Type your note:');
+            // rl.on('line', () => {
+            //     oNotes.add(rl.history[0]);
+            // })
+            // rl.resume();
+            console.clear();
+            rl.question('Type your note:', (note) => {
+                oNotes.add(note);
+            })
+            rl.clearLine();
             break;
         case 'remove':
-            oNotes.remove();
+            rl.question('Write ID of note you want to remove:', (noteId) => {
+                oNotes.remove(noteId);
+            });
             break;
         case 'clear':
             oNotes.clear();
@@ -23,12 +35,17 @@ rl.on('line', function(line) {
         case 'list':
             oNotes.list();
             break;
+        case 'stats':
+            oNotes.stats();
+            break;
         case 'help':
             oNotes.help();
             break;
         case 'exit':
             rl.close();
             break;
+        default:
+            console.log('Invalid input, type help to see commands.');
+            break;
     }
-
 })
--- note-app/notes.js
+++ note-app/notes.js
@@ -1,32 +1,58 @@
 import chalk from 'chalk';
 import fs from 'fs';
 
+function pause(time) {
+    let dt = new Date();
+    while (new Date()) - dt <= time;
+}
 export class Notes {
+
     constructor() {
-        fs.open('notes.json', 'r', (err) => {
-            if (err) {
-                fs.writeFileSync('notes.json', '');
-                console.log(chalk.green('Welcome in Notes-app.'));
-                console.log(chalk.cyan('Notes-app has created database to store your notes.'));
-                this.help();
-            } else {
-                console.log(chalk.green('Welcome in Notes-app.'));
-                this.help();
+
+        do {
+            try {
+                this.notesRaw = fs.readFileSync('notes.json');
+            } catch {
+                fs.writeFileSync('notes.json', '{}');
             }
-        })
+
+            try {
+                this.notesJson = JSON.parse(this.notesRaw);
+            } catch {
+                fs.writeFileSync('notes.json', '{}');
+                console.log(chalk.red('Database was corrupted. I\'ve created fresh database.'));
+            }
+        }
+        while (this.notesJson == undefined)
+        this.lastId = this.notesJson[Object.keys(this.notesJson).pop()]["id"];
     }
 
-    add() {
-        console.log('adding new note');
+    add(note) {
+        this.lastId++;
+        this.notesJson["note-" + this.lastId] = {
+            "id": this.lastId,
+            "note": note
+        };
+        fs.writeFileSync('notes.json', JSON.stringify(this.notesJson, null, 4));
+        console.clear();
+        console.log(chalk.green('Your note has beed added.'));
+        setTimeout(() => {
+            console.clear();
+        }, 2000);
     }
-    remove() {
-        console.log('removing specified note')
+    remove(noteId) {
+        delete this.notesJson["note-" + noteId];
+        fs.writeFileSync('notes.json', JSON.stringify(this.notesJson, null, 4));
+        console.log(this.notesJson);
     }
     clear() {
         console.log('cleaning all notes')
     }
     list() {
         console.log('listing all notes')
+    }
+    stats() {
+        console.log('show how many notes have been created since the beginning')
     }
     help() {
         console.log('.............................................................')
@@ -40,6 +66,8 @@ export class Notes {
         console.log(`${chalk.cyan('clear')} to remove all of your notes`);
         console.log();
         console.log(`${chalk.cyan('list')} to list all your notes`);
+        console.log();
+        console.log(`${chalk.cyan('stats')} to show how many note have been created since the beginning`);
         console.log();
         console.log(`${chalk.cyan('help')} to see this help recursion(recursion) ;-)`);
         console.log('................................................................')
